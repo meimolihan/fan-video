296998659bbb4dd2a6e95032ad73c2f4e4e91fe4
meimolihan <meimolihan@gmail.com>
2026-09-20 13:31:17 +0800

    日常更新

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -210,11 +210,29 @@ info "写入发版备注 RELEASE_NOTES.md"
     printf '%s\n' "${MSG}"
     printf '\n'
   fi
+  printf '```bash\n'
   printf 'docker pull mobufan/fan-video:latest\n'
+  printf '```\n'
+  printf '```bash\n'
   printf 'docker pull mobufan/fan-video:%s\n' "${TAG}"
+  printf '```\n'
   printf '\n'
+  printf '```bash\n'
   printf 'docker pull ghcr.io/meimolihan/fan-video:latest\n'
+  printf '```\n'
+  printf '```bash\n'
   printf 'docker pull ghcr.io/meimolihan/fan-video:%s\n' "${TAG}"
+  printf '```\n'
+  printf '\n'
+  printf '## 二进制安装\n'
+  printf '```bash\n'
+  printf 'bash -c "$(curl -sSL https://raw.githubusercontent.com/meimolihan/fan-video/main/scripts/install.sh)" -p 9060 -d /var/lib/fan-video\n'
+  printf '```\n'
+  printf '\n'
+  printf '## 二进制卸载\n'
+  printf '```bash\n'
+  printf 'bash -c "$(curl -sSL https://raw.githubusercontent.com/meimolihan/fan-video/main/scripts/uninstall.sh)" -y --purge\n'
+  printf '```\n'
 } > RELEASE_NOTES.md
 
 # ===================== Git 提交 & Tag =====================
